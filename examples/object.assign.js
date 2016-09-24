9ef0df5cd7407ea3dd5022b4e844ef812d072f46
nelsonic <contact.nelsonic+github@gmail.com>
2016-09-24 11:39:36 +0100

    adds Object.assign example for https://github.com/dwyl/goodparts/issues/221 #1

---
+++ examples/object.assign.js
@@ -0,0 +1,16 @@
+var compile = require('google-closure-compiler-js').compile;
+
+var flags = {
+  jsCode: [{src: `
+    var user = { name: 'Jack', email: 'jackiscuel@email.net' };
+    var meta = { height: '185', eyes: 'blue', hair: 'blonde' };
+    var social = { twitter: 'jackyboy', instagram: 'iamjack' };
+    // and combine them into a _new_ object with Object.assign:
+    var jack = Object.assign({}, user, meta, social);
+  `}],
+};
+var out = compile(flags);
+var filename = __filename.replace('.js', '.compiled.js');
+var toSource = require('tosource'); // this is not a Standard! Using just for illustration purposes!
+require('fs').writeFileSync(filename, toSource(out.compiledCode));
+console.info(out.compiledCode);  // will print 'var x = 3;\n'
